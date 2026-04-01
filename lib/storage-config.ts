b52b95fe1101fbd6983cf9b2da559c53ccaeb3a5
NoCry <nocry@theresnocry.com>
2026-04-01 14:48:37 -0300

    chore: update baseline-browser-mapping and fix STORAGE_BUCKET scope

--- lib/storage-config.ts
+++ lib/storage-config.ts
@@ -2,8 +2,9 @@
  * Configuração global do Storage
  * @deprecated Use STORAGE_BUCKET from '@/lib/constants' instead
  */
-import { STORAGE_BUCKET } from './constants'
-export { STORAGE_BUCKET }
+import { STORAGE_BUCKET as CONST_STORAGE_BUCKET } from '@/lib/constants'
+
+export const STORAGE_BUCKET = CONST_STORAGE_BUCKET
 
 /**
  * Log de diagnóstico (executar uma vez no init)
@@ -12,7 +13,7 @@ export function logStorageConfig() {
   if (typeof window === 'undefined') return
 
   console.log('[StorageCheck] SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL?.slice(0, 32))
-  console.log('[StorageCheck] Using bucket:', STORAGE_BUCKET)
+  console.log('[StorageCheck] Using bucket:', CONST_STORAGE_BUCKET)
 }
 
 
